709601f956ba7d9c009aeae9b910f2370dc5a7e3
Sleazy Gram <abdallah.ghanmi>
2022-11-24 08:42:01 +0100

    translation ok

--- components/firstblock.jsx
+++ components/firstblock.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import tobi from "../public/tobi.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { FormattedMessage, useIntl } from "react-intl";
 
 export const FirstBlock = () => {
+  const { locales } = useRouter();
+  
   return (
     <div className="mt-6 sm:h-[50rem] sm:w-full sm:mt-3 sm:flex sm:justify-center sm:pt-[8em] -skew-y-3 bg-wavefirst bg-cover bg-center bg-no-repeat dark:black">
       <div className="skew-y-3 flex-col items-center sm:w-[40%] dark:black">
         <h1 className="text-[46px] text-center font-black w-full leading-10 sm:mt-[10rem]  text-[#7645d9] sm:w-[70%] dark:black">
-          Discover an unique NFT collection. The famous Heavy Heads.
+          <FormattedMessage
+            id="firstblock"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
         </h1>
       </div>
       <div className="skew-y-3">
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -8,10 +8,18 @@ import MetamaskCon from "./metamask";
 import { ImSun } from "react-icons/im";
 import { FaRegMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
+import { useRouter } from "next/router";
+import { FormattedMessage, useIntl } from "react-intl";
 
 export const Navbar = ({ client, connect }) => {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
+  const { locales } = useRouter();
+
+  const intl = useIntl();
+
+  // const navbar1 = intl.formatMessage({ id: "navbar-1" });
 
   const renderThemeChanger = () => {
     if (!mounted) return null;
@@ -21,7 +29,7 @@ export const Navbar = ({ client, connect }) => {
     if (currentTheme === "dark") {
       return (
         <ImSun
-          className="w-10 h-10 text-yellow-500 "
+          className="w-5 h-5 text-yellow-500 "
           role="button"
           onClick={() => setTheme("light")}
         />
@@ -29,7 +37,7 @@ export const Navbar = ({ client, connect }) => {
     } else {
       return (
         <FaRegMoon
-          className="w-10 h-10 text-gray-900 "
+          className="w-5 h-5 text-gray-900 "
           role="button"
           onClick={() => setTheme("dark")}
         />
@@ -42,13 +50,33 @@ export const Navbar = ({ client, connect }) => {
   }, []);
 
   return (
-    <div className="flex w-full pt-4 sm:flex text-lg sm:w-[60%] sm:justify-between sm:m-auto sm:pt-[2em] ">
+    <div className="flex w-full pt-4 sm:flex text-lg sm:w-[60%] sm:justify-between sm:m-auto sm:pt-[2em] sm:mb-8 ">
       <Image src={logoo} width={100} height={50} alt="logo" />
-      <div className="sm:space-x-5 flex items-center">
-        <Link href="#third">Roadmap</Link>
-        <Link href="#fourth">Arts</Link>
-        <Link href="#fifth">Team</Link>
-        <Link href="#second">My account</Link>
+      <div className="sm:space-x-8 flex items-center">
+        <Link href="#third">
+          <FormattedMessage
+            id="navbar-1"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
+        </Link>
+        <Link href="#fourth" className="sm:pr-3">
+          <FormattedMessage className="sm:p-3"
+            id="navbar-2"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
+        </Link>
+        <Link href="#fifth">
+          <FormattedMessage
+            id="navbar-3"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
+        </Link>
+        <Link href="#second">
+          <FormattedMessage
+            id="navbar-4"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
+        </Link>
         <Link href="https://twitter.com/heavyheadsoff">
           <TiSocialTwitter className="cursor-pointer" size="30px" />
         </Link>
@@ -57,6 +85,13 @@ export const Navbar = ({ client, connect }) => {
         </Link>
         <MetamaskCon client={client} connect={connect} />
         {renderThemeChanger()}
+        <div className="sm:pr-4 sm:space-x-2">
+          {[...locales].sort().map((locale) => (
+            <Link key={locale} href="/" locale={locale}>
+              {locale}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
